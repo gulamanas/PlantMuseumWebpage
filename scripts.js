e1a4002c0efaed22382dd1dede9fff879294de0a
Gulam Anas <g.anas786@gmail.com>
2021-05-26 08:54:29 +0530

    update in branch

--- scripts.js
+++ scripts.js
@@ -16,46 +16,28 @@ navSlide();
 
 // Carousel functions ----------
 
-const carouselSlide = document.querySelector(".carousel__slide");
-const carouselImages = document.querySelectorAll(".carousel__slide img");
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
 
-//Buttons
-const prevBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-//Counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-
-carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-
-//Button Listener
-
-nextBtn.addEventListener("click", () => {
-  if (counter >= carouselImages.length - 1) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter++;
-  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-});
-
-prevBtn.addEventListener("click", () => {
-  if (counter <= 0) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter--;
-  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-});
-
-carouselSlide.addEventListener("transitionend", () => {
-  if (carouselImages[counter].id === "lastClone") {
-    carouselSlide.style.transition = "none";
-    counter = carouselImages.length - 2;
-    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-  }
-  if (carouselImages[counter].id === "firstClone") {
-    carouselSlide.style.transition = "none";
-    counter = carouselImages.length - counter;
-    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-  }
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 //Gallery functions --------
@@ -64,3 +46,20 @@ const showImg = (img) => {
   const image = document.querySelector(".gallery__box");
   image.src = img.src;
 };
+
+// Smooth Scroll JQuery
+
+$(".r-nav a").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const something = this.hash;
+
+    $("html , body").animate(
+      {
+        scrollTop: $(something).offset().top,
+      },
+      800
+    );
+  }
+});
